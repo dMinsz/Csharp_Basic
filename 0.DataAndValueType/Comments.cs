91f4bff9822cdbe78502f03548df84365e42a914
dMinsz <dsminzks@gmail.com>
2023-04-06 18:52:03 +0900

    Add Comments and Value Type lecture

---
+++ 0.DataAndValueType/Comments.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* 주석 : 코드에 영향을 주지 않는 텍스트 */
+
+// 기본 주석
+// 한줄 주석 : 한 줄의 텍스트를 주석으로 취급
+/* 범위 주석 : 시작(/*)과 끝(* /)까지의 텍스트를 주석으로 취급 */
+
+// 주석 응용
+/*
+ * 여러줄 주석을 사용하고싶으면
+ * 시작(/*) 후 줄바꿈(엔터키 입력) 을 하면 자동으로 여러줄 주석처리가됨
+ * 주의 마지막 * 이후에 / 해줘야한다.
+ */
+
+/// 자동완성 주석 : 
+/// 클래스 또는 함수 앞에서 /// 입력으로 자동완성이 된다.
+
+/// <summary>
+/// 이런식으로
+/// </summary>
+
+
+namespace _0.DataAndValueType
+{// 나중에 객체지향할때 자세히 말해줄것이다.
+    //지금은 _0.DataAndValueType 의 이름으로 묶여있다고 생각하자..
+    internal class Comments
+    {
+        
+    }
+}
